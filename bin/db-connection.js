e746f301ba91f36f6afdfafefdafe299c0607368
Muhammed Shah <shah@pop-os.localdomain>
2020-05-17 14:49:03 +0530

    code clean up

--- bin/db-connection.js
+++ bin/db-connection.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/url-shortener', {
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
-}).then(event => console.log("Database connection established"))
-    .catch((err) => {
-        console.log("Database Connection Failed!\n", err.message);
-    });
+const MongoClient = require('mongodb').MongoClient;
+let mongoURI = "mongodb://localhost:27017/url-shortener";
+
+if (process.env.NODE_ENV === 'production') {
+    mongoURI = "mongodb+srv://<username>:<password>@cluster0-ymzcl.mongodb.net/test?retryWrites=true&w=majority";
+    const client = new MongoClient(mongoURI, {useNewUrlParser: true});
+    client.connect().then(console.log("Production Database connection established"))
+        .catch((err) => {
+            console.log("Database Connection Failed!\n", err.message);
+        });
+} else {
+    mongoose.connect(mongoURI, {
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }).then(event => console.log("Database connection established"))
+        .catch((err) => {
+            console.log("Database Connection Failed!\n", err.message);
+        });
+
+}
 
 module.exports = mongoose.connect;
