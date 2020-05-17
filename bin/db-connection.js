577f190c3a12b44611d9ea6241baab0d41273fc7
Muhammed Shah <shah@pop-os.localdomain>
2020-05-17 15:05:22 +0530

    code clean up

--- bin/db-connection.js
+++ bin/db-connection.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 let mongoURI = "mongodb://localhost:27017/url-shortener";
 
 if (process.env.NODE_ENV === 'production') {
-    mongoURI = "mongodb+srv://<username>:<password>@cluster0-ymzcl.mongodb.net/test?retryWrites=true&w=majority";
+    mongoURI = "mongodb+srv://user:shah@cluster0-ymzcl.mongodb.net/test?retryWrites=true&w=majority";
     const client = new MongoClient(mongoURI, {useNewUrlParser: true});
     client.connect().then(console.log("Production Database connection established"))
         .catch((err) => {
