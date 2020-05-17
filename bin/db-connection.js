fe5f4a18fac02c0b80138e41ef437f82b4765a9d
Muhammed Shah <shah@pop-os.localdomain>
2020-05-17 15:09:04 +0530

    fix production DB

--- bin/db-connection.js
+++ bin/db-connection.js
@@ -4,8 +4,12 @@ let mongoURI = "mongodb://localhost:27017/url-shortener";
 
 if (process.env.NODE_ENV === 'production') {
     mongoURI = "mongodb+srv://user:shah@cluster0-ymzcl.mongodb.net/test?retryWrites=true&w=majority";
-    const client = new MongoClient(mongoURI, {useNewUrlParser: true});
-    client.connect().then(console.log("Production Database connection established"))
+    mongoose.connect(mongoURI, {
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }).then(event => console.log("Database connection established"))
         .catch((err) => {
             console.log("Database Connection Failed!\n", err.message);
         });
